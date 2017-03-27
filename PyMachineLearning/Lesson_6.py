d32a826901cb3879d2783001d496f203d2b3b45b
Kieran McGregor <kieranmcgregor@gmail.com>
2017-03-27 06:15:54 -0400

    Lesson 6 modification

--- PyMachineLearning/Lesson_6.py
+++ PyMachineLearning/Lesson_6.py
@@ -2,16 +2,21 @@ from sklearn import metrics, model_selection
 import tensorflow as tf
 from tensorflow.contrib import learn
 
-def main(unused_argv):
+def main():
     # Load dataset
     iris = learn.datasets.load_dataset("iris")
     x_train, x_test, y_train, y_test = model_selection.train_test_split(
         iris.data, iris.target, test_size = 0.2, random_state = 42)
 
     # Build 3 layer DNN with 10, 20, 10 units,respectiely
-    classifier = learn.DNNClassifier(hidden_units = [10, 20, 10], n_classes = 3)
+    feature_columns = [ tf.contrib.layers.real_valued_column( "", dimension = 4 ) ]
+    classifier = learn.DNNClassifier(feature_columns=feature_columns,
+                                    hidden_units=[10, 20, 10],
+                                    n_classes=3)
 
     # Fit and predict
     classifier.fit(x_train, y_train, steps = 200)
     score = metrics.accuracy_score(y_test, classifier.predict(x_test))
     print('Accuracy: {0:f}'.format(score))
+
+main()
