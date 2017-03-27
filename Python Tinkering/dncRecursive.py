519fe90a32263a7b953d0898aac79fc93f6fa8ed
Kieran McGregor <kieranmcgregor@gmail.com>
2017-03-27 06:17:16 -0400

    Divide and conquer recursive attempt

--- Python Tinkering/dncRecursive.py
+++ Python Tinkering/dncRecursive.py
@@ -10,10 +10,7 @@ def dnc_recur(array_1, array_2):
 
     if (len(array_1) == 1 & len(array_2) == 1):
         print (array_12, array_22)
-        if (array_12 < array_22):
-            return [array_12, array_22]
-        else:
-            return [array_22, array_12]
+        return (array_12, array_22)
 
     elif (half_1 == 0):
         return (dnc_recur(array_12, array_21),
@@ -24,13 +21,6 @@ def dnc_recur(array_1, array_2):
                 dnc_recur(array_12, array_22))
 
     else:
-        array_11 = array_1[:half_1]
-        array_12 = array_1[half_1:]
-
-        array_21 = array_2[:half_2]
-        array_22 = array_2[half_2:]
-
-        print (array_11, array_12, array_21, array_22)
         return (dnc_recur(array_11, array_21),
                 dnc_recur(array_11, array_22),
                 dnc_recur(array_12, array_21),
