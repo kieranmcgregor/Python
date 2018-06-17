1da398d47c627aba9f0201678843857816f4834a
Kieran McGregor <kieranmcgregor@gmail.com>
2018-06-17 13:54:35 +0900

    Latest Linear Regression example

--- ML4HB/linear_regression/line_reg.py
+++ ML4HB/linear_regression/line_reg.py
@@ -2,11 +2,58 @@ import numpy as npy
 import matplotlib.pyplot as plt
 import csv
 
+class LinearRegression(object):
+	# Implements Linear Regression
+	
+	def __init__(self):
+		self.w = 0
+		self.b = 0
+		self.rho = 0
+	
+	def fit(self, X, y):
+		mean_x = X.mean()
+		mean_y = y.mean()
+		
+		errors_x = X - mean_x
+		errors_y = y - mean_y
+		
+		errors_product_xy = npy.sum(npy.multiply(errors_x, errors_y) )
+		squared_errors_x = npy.sum(errors_x ** 2)
+		
+		self.w = errors_product_xy / squared_errors_x
+		self.b = mean_y - self.w * mean_x
+		
+		N = len(X)
+		std_x = X.std()
+		std_y = y.std()
+		
+		covariance = errors_product_xy / N
+		self.rho = covariance / (std_x * std_y)
+	
+	def predict(self, X):
+		return self.w * X + self.b
+
+def visualize_solution(X, y, lin_reg):
+	visualize_dataset(X, y)
+	
+	x = npy.arange(0, 800)
+	y = lin_reg.predict(x)
+	plt.plot(x, y, 'r--', label = 'r = %.2f' %lin_reg.rho)
+	plt.legend()
+	
+	plt.show()
+	
+def visualize_dataset(X, y):
+	plt.xlabel('Number of shares')
+	plt.ylabel('Number of likes')
+	plt.scatter(X, y)
+	# plt.show()
+
 def load_dataset():
 	num_rows = sum(1 for line in open('dataset_Facebook.csv')) - 1
 	
-	X = npy.zeroes( (num_rows, 1) )
-	y = npy.zeroes( (num_rows, 1) )
+	X = npy.zeros( (num_rows, 1) )
+	y = npy.zeros( (num_rows, 1) )
 	
 	with open('dataset_Facebook.csv') as f:
 		reader = csv.DictReader(f, delimiter = ';')
@@ -18,13 +65,10 @@ def load_dataset():
 			
 	return X, y
 	
-
-def visualize_dataset(X, y):
-	plt.xlabel('Number of shares')
-	plt.ylabel('Number of likes')
-	plt.scatter(X, y)
-	plt.show()
-	
 if __name__ == '__main__':
 	X, y = load_dataset()
-	visualize_dataset(X, y)
+	# visualize_dataset(X, y)
+	lin_reg = LinearRegression()
+	lin_reg.fit(X, y)
+	
+	visualize_solution(X, y, lin_reg)
